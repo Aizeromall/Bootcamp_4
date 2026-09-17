139e846193bfad06f6314800703aee5fa6021e76
byeongjae <sweetday0814@gmail.com>
2026-09-17 19:23:15 +0900

    base

---
+++ lib/view/insert_todo_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InsertTodoList extends StatefulWidget {
+  const InsertTodoList({super.key});
+
+  @override
+  State<InsertTodoList> createState() => _InsertTodoListState();
+}
+
+class _InsertTodoListState extends State<InsertTodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/view/update_todo_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateTodoList extends StatefulWidget {
+  const UpdateTodoList({super.key});
+
+  @override
+  State<UpdateTodoList> createState() => _UpdateTodoListState();
+}
+
+class _UpdateTodoListState extends State<UpdateTodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
